0eeaeae8438273989e7a154099c9a7030ebba6d5
Haithamsaleh <alawaji.haitham@gmail.com>
2022-07-22 22:00:54 +0300

    connect backend use axios

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -2,55 +2,74 @@ import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom";
 import 'antd/dist/antd.css';
 import { UploadOutlined } from '@ant-design/icons';
+import axios from "axios";
+import { useSelector } from "react-redux";
+import { useParams } from "react-router";
 import { EditOutlined, EllipsisOutlined, SettingOutlined } from '@ant-design/icons';
 import { Avatar, Card, Skeleton, Switch  } from 'antd';
-const { Meta } = Card;
+
+const BASE_URL = 'http://localhost:4000';
+
 
 
 const Home = () => {
-    const [loading, setLoading] = useState(true);
+  const { id } = useParams();
+  const [Task, setTask] = useState([]);
 
-    const onChange = (checked) => {
-      setLoading(!checked);
-    };
+
+
   
-    return (
-      <>
+    
+    useEffect(() => {
+      getTask();
+    },[]);
   
-        <Card
-          style={{
-            width: 300,
-            marginTop: 16,
-          }}
-          loading={loading}
-        >
-          <Meta
-            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-            title="Card title"
-            description="This is the description"
-          />
-        </Card>
   
-        <Card
-          style={{
-            width: 300,
-            marginTop: 16,
-          }}
-          actions={[
-            <SettingOutlined key="setting" />,
-            <EditOutlined key="edit" />,
-            <EllipsisOutlined key="ellipsis" />,
-          ]}
-        >
-          <Skeleton loading={loading} avatar active>
-            <Meta
-              avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
-              title="Card title"
-              description="This is the description"
-            />
-          </Skeleton>
-        </Card>
-      </>
-    );
+
+
+
+  
+  const getTask = async (id) => {
+    try {
+      const result = await axios.get(`${BASE_URL}/tasks`);
+console.log(result.data);
+      setTask(result.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+return (
+  <>
+  
+  {Task.map((item, I) => {
+    
+    if (item.complete == false && item.isdone == false) {
+      return <h2 key={I}>
+           <h2>name: {item.task}</h2>
+            <h2>date: {item.date}</h2>
+            <p>{item.importance}</p>
+            <hr/>
+      </h2>;
+    }else{
+        return (
+          
+          
+          <div key={I}>
+            
+            <h2 style={{color: "red"}}>name: {item.task}</h2>
+            <h2 style={{color: "red"}}>date: {item.date}</h2>
+            <p style={{color: "red"}}>{item.importance}</p>
+            
+ 
+
+
+
+            <hr />
+          </div>
+        );}
+      })}
+  </>
+  );
   };
   export default Home;
+  // stoped here 
