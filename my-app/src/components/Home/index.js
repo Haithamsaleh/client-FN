731367c1fca22129cfd403297084a2922daf8e18
Haithamsaleh <alawaji.haitham@gmail.com>
2022-07-27 21:34:14 +0300

    steps!  & form!

--- my-app/src/components/Home/index.js
+++ my-app/src/components/Home/index.js
@@ -5,15 +5,17 @@ import { UploadOutlined } from '@ant-design/icons';
 import axios from "axios";
 import { useSelector } from "react-redux";
 import { useParams } from "react-router";
-import { EditOutlined, EllipsisOutlined, SettingOutlined, DeleteOutlined , CheckOutlined, DownloadOutlined } from '@ant-design/icons';
-import { Avatar, Card, Skeleton, Switch ,Button,  Drawer , Input , DatePicker} from 'antd';
+import { EditOutlined, EllipsisOutlined, SettingOutlined, DeleteOutlined, CheckOutlined, DownloadOutlined } from '@ant-design/icons';
+import { Avatar, Card, Skeleton, Switch, Button, Drawer, Input, DatePicker, Select, Steps, Row, Col, Divider, } from 'antd';
 import './style.css'
+
 import Swal from "sweetalert2";
 
 
 const BASE_URL = 'http://localhost:4000';
 
 
+const { Step } = Steps;
 
 const Home = () => {
   const { id } = useParams();
@@ -27,58 +29,64 @@ const Home = () => {
   const [importance, setImportance] = useState("");
 
 
+  const [current, setCurrent] = useState(0);
 
-  
-    
-    useEffect(() => {
-      getTask();
-    },[]);
-  
-    const showDrawer = () => {
-      setVisible(true);
-    };
-  
-    const onClose = () => {
-      setVisible(false);
-    };
 
-const newTask = async () => {
-  try { 
-    await axios.post(`${BASE_URL}/newtask`,
-    {
-      task: tasks,
-      completeBy: completeBy,
-      importance: importance,
-    },
-      //{
-      // headers :{
-      //   Authorization: `Bearer ${state.token}`,
-      // }
-    )
-    Swal.fire({
-      position: "center",
-      icon: "success",
-      title: "task successfule ",
-      showConfirmButton: false,
-      timer: 2500,
-    });
 
-  } catch (error) {
-    Swal.fire({
-      position: "center",
-      icon: "error",
-      title: "Opss...! ,something wrong",
-      showConfirmButton: false,
-      timer: 2500,
-    });
+  useEffect(() => {
+    getTask();
+  }, []);
+
+  const showDrawer = () => {
+    setVisible(true);
+  };
+
+  const onClose = () => {
+    setVisible(false);
+  };
+
+  const onChange = (value) => {
+    console.log('onChange:', current);
+    setCurrent(value);
+  };
+
+  const newTask = async () => {
+    try {
+      await axios.post(`${BASE_URL}/newtask`,
+        {
+          task: tasks,
+          completeBy: completeBy,
+          importance: importance,
+        },
+        //{
+        // headers :{
+        //   Authorization: `Bearer ${state.token}`,
+        // }
+      )
+      Swal.fire({
+        position: "center",
+        icon: "success",
+        title: "task successfule ",
+        showConfirmButton: false,
+        timer: 2500,
+      });
+
+    } catch (error) {
+      Swal.fire({
+        position: "center",
+        icon: "error",
+        title: "Opss...! ,something wrong",
+        showConfirmButton: false,
+        timer: 2500,
+      });
+    }
   }
-}
 
-  
+
   const getTask = async (id) => {
     try {
       const result = await axios.get(`${BASE_URL}/tasks`);
-console.log(result.data);
+      console.log(result.data);
       setTask(result.data);
     } catch (error) {
       console.log(error);
@@ -91,125 +99,148 @@ console.log(result.data);
     getTask();
   };
 
-const  delTask = async (id) => { 
-  const res = await axios.delete(`${BASE_URL}/deltasks/${id}`,{
-  });
-  getTask();
-}; 
+  const delTask = async (id) => {
+    const res = await axios.delete(`${BASE_URL}/deltasks/${id}`, {
+    });
+    getTask();
+  };
 
 
 
 
-return (
-  <>
-   <div className="site-drawer-render-in-current-wrapper">
-      Render in this
-      <div
-        style={{
-          marginTop: 16,
-        }}
-      >
-        <Button type="primary" onClick={showDrawer}>
-          Open
-        </Button>
-      </div>
-      <Drawer
-        title="Basic Drawer"
-        placement="right"
-        closable={false}
-        onClose={onClose}
-        visible={visible}
-        getContainer={false}
-        width={'100%'}
-        style={{
-          position: 'absolute',
-        }}
-      >
-<Input
-                    bg="white"
-                      id="text"
-                      type="text"
-                      onChange={(e) => setTasks(e.target.value)}
-                    />    
-                      <DatePicker  onChange={(e) => setCompleteBy(e.target)} />
-                   <Input
-                    bg="white"
-                      id="text"
-                      type="text"
-                      onChange={(e) => setImportance(e.target.value)}
-                    />  
-                        <Button type="primary" onClick={onClose}>
-        Close
-        </Button>
-        <Button
-                  onClick={newTask}
-                >
-                  {" "}
-                  Post
-                </Button>
-      </Drawer>
-    </div>
-        <Button type="primary" shape="round" icon={<DownloadOutlined />}  />
-        <hr/>
-
-  {Task.map((item, I) => {
+  return (
+    <>
+     
     
-    // console.log(item._id);
-    if (item.complete == false && item.isdone == false) {
-      return <h2 key={I}>
-           <h2>name: {item.task}</h2>
+
+    
+      <div className="site-drawer-render-in-current-wrapper">
+        Render in this
+        <div
+          style={{
+            marginTop: 16,
+          }}
+        >
+          <Button type="primary" onClick={showDrawer}>
+            Open
+          </Button>
+        </div>
+        <Drawer
+          title="Basic Drawer"
+          placement="right"
+          closable={false}
+          onClose={onClose}
+          visible={visible}
+          getContainer={false}
+          width={'100%'}
+          style={{
+            position: 'absolute',
+          }}
+        >
+          <Input
+            placeholder='newtask'
+            bg="white"
+            id="text"
+            type="text"
+            onChange={(e) => setTasks(e.target.value)}
+          />
+          <DatePicker placeholder='Date com' onChange={(e) => setCompleteBy(e.target)} />
+          <Input
+            placeholder='impo'
+            bg="white"
+            id="text"
+            type="text"
+            onChange={(e) => setImportance(e.target.value)}
+          />
+          <Button type="primary" onClick={onClose}>
+            Close
+          </Button>
+          <Button
+            onClick={newTask}
+          >
+            {" "}
+            Post
+          </Button>
+        </Drawer>
+      </div>
+      <Button type="primary" shape="round" icon={<DownloadOutlined />} />
+      <hr />
+
+      {Task.map((item, I) => {
+
+        // console.log(item._id);
+        if (item.complete == false && item.isdone == false) {
+          return <div key={I}>
+            <h2>name: {item.task}</h2>
             <h2>date: {item.date}</h2>
             <p>{item.importance}</p>
-            
-        <Button icon={<CheckOutlined style={{ color: "white" }}/>}
+            <br/>
+            <Steps current={current} onChange={onChange}>
+        <Step title="Step 1" description="This is a description." />
+        <Step title="Step 2" description="This is a description." />
+        <Step title="Step 3" description="This is a description." />
+        <Step  onClick={() => {
+                taskdone(item._id);
+              }} title="Step 3" description="This is a description." />
+      </Steps>
+            <Button icon={<CheckOutlined style={{ color: "white" }} />
+            }
 
-style={{ background: "green", borderColor: "white" }}
-onClick={() => {
-  taskdone(item._id);
-}}
+              style={{ background: "green", borderColor: "white" }}
+              onClick={() => {
+                taskdone(item._id);
+              }}
 
-/>
-<Button icon={<DeleteOutlined style={{ color: "white" }}/>}
+            />
+               
+            <Button icon={<DeleteOutlined style={{ color: "white" }} />}
 
-style={{ background: "red", borderColor: "white" }}
-onClick={() => {
-  delTask(item._id);
-}}
+              style={{ background: "red", borderColor: "white" }}
+              onClick={() => {
+                delTask(item._id);
+              }}
 
-/>
-<hr/>
-
-      </h2>;
-    }else{
-        return (
-          
-          
-          <div key={I}>
-            
-            <h2 style={{color: "red"}}>name: {item.task}</h2>
-            <h2 style={{color: "red"}}>date: {item.date}</h2>
-            <p style={{color: "red"}}>{item.importance}</p>
-            
-            
-
- 
-
-            <Button icon={<DeleteOutlined style={{ color: "white" }}/>}
-
-style={{ background: "red", borderColor: "white" }}
-onClick={() => {
-  delTask(item._id);
-}}
-
-/>
-
+            />
             <hr />
-          </div>
-        );}
+
+            <br />
+
+
+          </div>;
+
+        } else {
+          return (
+
+
+            <div key={I}>
+                <del>
+
+              <h2 style={{ color: "red" }}>name: {item.task}</h2>
+              <h2 style={{ color: "red" }}>date: {item.date}</h2>
+              <p style={{ color: "red" }}>{item.importance}</p>
+                </del>
+
+
+
+
+
+              <Button icon={<DeleteOutlined style={{ color: "white" }} />}
+
+                style={{ background: "red", borderColor: "white" }}
+                onClick={() => {
+                  delTask(item._id);
+                }}
+
+              />
+
+              <hr />
+            </div>
+          );
+        }
 
       })}
-  </>
+    </>
   );
-  };
-  export default Home;
-//stoped here need to make  form for addtask 
+};
+export default Home;
+//stoped here :bug(all steps move together) 
